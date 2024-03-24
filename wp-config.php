296dba34d71381c3773a37ad3ad10aad472bb8c4
Mohd Suhail <40312886+Suhailsaif@users.noreply.github.com>
2024-03-24 18:22:44 +0530

    Update wp-config.php

--- wp-config.php
+++ wp-config.php
@@ -20,13 +20,13 @@
 
 // ** Database settings - You can get this info from your web host ** //
 /** The name of the database for WordPress */
-define( 'DB_NAME', 'wpresources' );
+define( 'DB_NAME', 'inventor_wpresources' );
 
 /** Database username */
-define( 'DB_USER', 'root' );
+define( 'DB_USER', 'inventor_admin' );
 
 /** Database password */
-define( 'DB_PASSWORD', '' );
+define( 'DB_PASSWORD', 'SNM@123!@#' );
 
 /** Database hostname */
 define( 'DB_HOST', 'localhost' );
